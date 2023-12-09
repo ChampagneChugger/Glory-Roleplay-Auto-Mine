f9a1e44b883af451db8578075a85842717d52a90
Mario Fučkan <mario.fucchan15@gmail.com>
2023-12-09 05:34:20 +0100

    New version of the Glory Automine

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Glory-Roleplay-Auto-Mine
 ## Requirements:
-Python 3+, pyautogui, keyboard
+Python 3+, numpy, mss, ctypes, keyboard, time, os, pyautogui
 ## How to use:
 1. Download the script
 2. Open GTA V FiveM and join Glory Roleplay
 3. Get to a mining site
 4. Open cmd and go to the script location
 5. Type: python bot.py and press enter
-6. The script will start in 4 seconds, in the meanwhile return to GTA
-7. Start digging and the script should automatically click "e" when the red line hovers over the blue background.
+6. The script will start in 5 seconds, in the meanwhile return to GTA
+7. Equid a pickaxe and wait for the script to start. Script should automatically click "e" when the red line hovers over the blue background.
 
 # Note: This has only been tested with 1920x1080 resolution.
 
--- bot.py
+++ bot.py
@@ -1,53 +1,140 @@
-from pyautogui import *
-import pyautogui
-import time
+import numpy as np
+import mss.tools
+import ctypes
 import keyboard
+import time
+import os
+import pyautogui
 
-pozicijeplavog = [
-    [925, 502],
-    [941, 492],
-    [959, 486],
-    [976, 491],
-    [989, 497],
-    [1001, 511],
-    [1005, 552],
-    [997, 569],
-    [985, 581],
-    [966, 587],
-    [950, 588],
-    [935, 583],
-    [929, 577],
-    [917, 566],
-    [912, 557],
-    [908, 543],
-    [913, 516],
-    [921, 504],
-    [926, 499],
+# Get user screen resolution
+user32 = ctypes.windll.user32
+screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+# Possible position of blue pixels
+bluepositions = [
+    [57, 4],
+    [68, 6],
+    [77, 10],
+    [85, 15],
+    [93, 21],
+    [98, 29],
+    [103, 39],
+    [105, 49],
+    [105, 61],
+    [103, 70],
+    [99, 80],
+    [94, 87],
+    [87, 93],
+    [79, 99],
+    [70, 103],
+    [62, 105],
+    [54, 105],
+    [45, 104],
+    [37, 101],
+    [30, 97],
+    [22, 91],
+    [16, 85],
+    [12, 77],
+    [8, 69],
+    [6, 58],
+    [6, 48],
+    [9, 38],
+    [13, 29],
+    [17, 23],
+    [23, 17],
+    [30, 11],
+    [37, 8],
+    [45, 5],
+    [51, 4],
 ]
 
+boxwidth = 110
+boxheight = 110
 
-svepozplavog = []
+# Screenshot capture area
+capturearea = {
+    "top": int(screensize[1] / 2) - int(boxheight / 2),
+    "left": int(screensize[0] / 2) - int(boxwidth / 2),
+    "width": boxwidth,
+    "height": boxheight,
+}
+
+# Array containing valid pixel positions
+arrayWithBlues = []
+
+# Clear cmd
+clear = lambda: os.system("cls")
 
 
-def pozplavog():
-    for x in pozicijeplavog:
-        if pyautogui.pixel(x[0], x[1])[2] == 194:
-            svepozplavog.extend([[x[0], x[1]]])
+def getValidPixels():
+    time.sleep(0.1)
+    pyautogui.mouseDown()
+    time.sleep(0.1)
+    pyautogui.mouseUp()
+    with mss.mss() as sct:
+        img = sct.grab(capturearea)
+        imgarray = np.array(img)
 
-    print(svepozplavog)
+        for pixels in bluepositions:
+            currentPixelArray = imgarray[pixels[1]][pixels[0]]
+
+            if (
+                currentPixelArray[0] == 194
+                and currentPixelArray[1] == 113
+                and currentPixelArray[2] == 25
+            ):
+                arrayWithBlues.append([pixels[1], pixels[0]])
+
+    print("Blues here: ", arrayWithBlues)
 
 
-time.sleep(4)
+# Wait 4 seconds before running script
+def startTimer(seconds):
+    while seconds > 0:
+        print("Starting in...", seconds)
+        seconds -= 1
+        time.sleep(1)
+        clear()
 
-while keyboard.is_pressed("q") == False:
-    if len(svepozplavog) == 0:
-        pozplavog()
+    print("Started")
 
-    while len(svepozplavog) != 0:
-        for x in svepozplavog:
-            if pyautogui.pixel(x[0], x[1])[0] == 255:
-                print(x[0], x[1])
-                pyautogui.keyDown("e")
-                time.sleep(0.01)
-                pyautogui.keyUp("e")
-                svepozplavog = []
+
+def doSomething():
+    print("Started clicking function.")
+    clicked = False
+    timeout = 0
+    with mss.mss() as sct:
+        while clicked == False:
+            img = sct.grab(capturearea)
+            imgarray = np.array(img)
+
+            timeout += 1
+
+            if timeout > 1000:
+                break
+
+            for pixels in arrayWithBlues:
+                currentPixelArray = imgarray[pixels[0]][pixels[1]]
+                print(pixels)
+
+                if (
+                    currentPixelArray[0] == 0
+                    and currentPixelArray[1] == 0
+                    and currentPixelArray[2] == 255
+                ):
+                    pyautogui.keyDown("e")
+
+                    pyautogui.keyUp("e")
+                    clicked = True
+                    print("Clicked")
+
+    arrayWithBlues.clear()
+
+
+startTimer(5)
+
+while not keyboard.is_pressed("q"):
+    if len(arrayWithBlues) > 0:
+        doSomething()
+    else:
+        getValidPixels()
